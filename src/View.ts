6cb0aae33d323abfa486e617e91444ae11747750
Fredrik Kemling <kemling@gmail.com>
2023-04-30 18:25:34 +0200

    moving canvas operations to View.ts

---
+++ src/View.ts
@@ -0,0 +1,115 @@
+export class View {
+
+    private width: number;
+    private height: number;
+    private cellWidth: number = 3;
+    private _canvas: HTMLCanvasElement;
+    private _ctx: CanvasRenderingContext2D;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this._canvas = canvas;
+        this.height = this._canvas.offsetHeight;
+        this.width = this._canvas.offsetWidth;
+        this._ctx = this._canvas.getContext('2d');
+    }
+
+
+
+    decreaseCellSize(): void {
+        if (this.cellWidth > 1) {
+            this.cellWidth -= 3;
+        }
+    }
+
+    increaseCellSize(): void {
+        if (this.cellWidth < 40) {
+            this.cellWidth += 3;
+        }
+    }
+
+    drawPolyomino(arrayOfShapes, arrayOfSymmetryInfo): void {
+        const vitKant: number = 0.5;
+        let offset_x: number = this.cellWidth;
+        let offset_y: number = this.cellWidth;
+        //max h�jd f�r aktuel rad. Anv�nds f�r att ekonomisera utrymmet
+        let maxShapeHeight: number = 0;
+
+
+
+        // tar omr�det som kan ritas ut p�
+
+        if (!this._canvas.getContext) {
+            return;
+        }
+
+
+        this._ctx.clearRect(0, 0, this.width, this.height);
+        this._ctx.fillStyle = 'rgba(170,170,170,1)';
+        this._ctx.save();
+        this._ctx.translate(offset_x, offset_y);
+
+        //f�r alla shapes (att rita ut)
+        for (let z: number = 0; z < arrayOfShapes.length; z++) {
+            const formen = arrayOfShapes[z];
+            this._ctx.fillStyle = this.getFillStyle(arrayOfSymmetryInfo[z]);
+
+
+            //ritar ut 
+            for (let i: number = 0; i < formen.length; i++) {
+                //sparar max antal kvadrater p� h�jden
+                if (formen.length > maxShapeHeight) { maxShapeHeight = formen.length; }
+                //ritar ut alla bitar
+                for (let j: number = 0; j < formen[0].length; j++) {
+                    if (formen[i][j] > 0) {
+                        this._ctx.fillRect(
+                            j * this.cellWidth,
+                            i * this.cellWidth,
+                            this.cellWidth - vitKant,
+                            this.cellWidth - vitKant);
+                    }
+                }
+
+            }
+            //om bredden �r n�dd till h�ger. s� ska det bli en ny rad
+            offset_x += (formen[0].length + 1) * this.cellWidth;
+            if (offset_x > this.width - (formen[0].length + 1) * this.cellWidth) {
+                offset_x = this.cellWidth;
+                offset_y += (maxShapeHeight + 1) * this.cellWidth;
+                maxShapeHeight = 0;
+
+            }
+            //}
+
+            this._ctx.restore();
+            this._ctx.save();
+            this._ctx.translate(offset_x, offset_y);
+        }
+        this._ctx.restore();
+
+    }
+
+    private getFillStyle(symmetryNumber: number): string {
+        switch (symmetryNumber) {
+            case 0:
+                return 'rgba(170,170,170,1)';
+            case 1:
+            case 2:
+                return 'rgba(221,153,153,1)';
+            case 4:
+            case 8:
+                return 'rgba(170,221,170,1)';
+            case 16:
+                return 'rgba(170,170,221,1)';
+            case 19:
+                return 'rgba(204,170,204,1)';
+            case 28:
+                return 'rgba(255,153,119,1)';
+            case 48:
+                return 'rgba(255,204,102,1)';
+            case 63:
+                return 'rgba(136,204,204,1)';
+            default:
+                return 'rgba(0,0,255,1)';
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -18,27 +18,29 @@ import {
     yAxisSymmetry
 } from './MatrixOperations';
 import './style.css';
+import { View } from './View';
 
 document.getElementById('startButton').addEventListener('click', () => starta());
-document.getElementById('plusButton').addEventListener('click', () => plusKvadratBredd());
-document.getElementById('minusButton').addEventListener('click', () => minusKvadratBredd());
+document.getElementById('plusButton').addEventListener('click', () => increaseSize());
+document.getElementById('minusButton').addEventListener('click', () => decreaseSize());
 document.getElementById('generationListan').addEventListener('change', () => listChange());
+const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 
-let WIDTH;
-let HEIGHT;
-let kvadratBredd = 3;
-let variantGeneration = new Array();
-let generationLevel = new Array();
-let startMatris = new Array();
-let calculationTime = new Array();
+const view: View = new View(canvas);
+
+
+let variantGeneration = [];
+let generationLevel = [];
+let startMatris = [];
+const calculationTime: string[] = [];
 calculationTime.push("0s");
 startMatris.push([1]);
 generationLevel.push(startMatris);
 variantGeneration.push(generationLevel);
 let startMeasuredTime: number = 0;
 
-let variantGenerationSymInfo = new Array();
-let generationLevelSymInfo = new Array();
+const variantGenerationSymInfo = [];
+let generationLevelSymInfo = [];
 generationLevelSymInfo.push([1]);
 variantGenerationSymInfo.push(generationLevelSymInfo);
 
@@ -52,92 +54,13 @@ function drawShapes(): void {
 
     const valdGeneration: number = getListValue();
 
-    //arrayen som formerna ligger i
     let shapesArray = variantGeneration[valdGeneration - 1];
-    //arrayen som talar om vilken symmetri formen har
     let symmetryArray = variantGenerationSymInfo[valdGeneration - 1];
 
-    let vitKant = 0;
-    let offset_x = kvadratBredd;
-    let offset_y = kvadratBredd;
-    //max h�jd f�r aktuel rad. Anv�nds f�r att ekonomisera utrymmet
-    let maxShapeHeight = 0;
+    view.drawPolyomino(shapesArray, symmetryArray);
 
-
-    const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-    // tar omr�det som kan ritas ut p�
-    HEIGHT = canvas.offsetHeight;
-    WIDTH = canvas.offsetWidth;
-    if (canvas.getContext) {
-
-        let ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, WIDTH, HEIGHT);
-        ctx.fillStyle = 'rgba(170,170,170,1)';
-        ctx.save();
-        ctx.translate(offset_x, offset_y);
-
-        //f�r alla shapes (att rita ut)
-        for (let z: number = 0; z < shapesArray.length; z++) {
-            const formen = shapesArray[z];
-            ctx.fillStyle = getFillStyle(symmetryArray[z]);
-
-
-            //if(symmetryArray[z]!==0){
-            //ritar ut 
-            for (let i: number = 0; i < formen.length; i++) {
-                //sparar max antal kvadrater p� h�jden
-                if (formen.length > maxShapeHeight) { maxShapeHeight = formen.length; }
-                //ritar ut alla bitar
-                for (let j: number = 0; j < formen[0].length; j++) {
-                    if (formen[i][j] > 0) {
-                        ctx.fillRect(j * kvadratBredd, i * kvadratBredd, kvadratBredd - vitKant, kvadratBredd - vitKant);
-                    }
-                }
-
-            }
-            //om bredden �r n�dd till h�ger. s� ska det bli en ny rad
-            offset_x += (formen[0].length + 1) * kvadratBredd;
-            if (offset_x > WIDTH - (formen[0].length + 1) * kvadratBredd) {
-                offset_x = kvadratBredd;
-                offset_y += (maxShapeHeight + 1) * kvadratBredd;
-                maxShapeHeight = 0;
-
-            }
-            //}
-
-            ctx.restore();
-            ctx.save();
-            ctx.translate(offset_x, offset_y);
-        }
-        ctx.restore();
-    }
 }
 
-
-function getFillStyle(symmetryNumber: number): string {
-    switch (symmetryNumber) {
-        case 0:
-            return 'rgba(170,170,170,1)';
-        case 1:
-        case 2:
-            return 'rgba(221,153,153,1)';
-        case 4:
-        case 8:
-            return 'rgba(170,221,170,1)';
-        case 16:
-            return 'rgba(170,170,221,1)';
-        case 19:
-            return 'rgba(204,170,204,1)';
-        case 28:
-            return 'rgba(255,153,119,1)';
-        case 48:
-            return 'rgba(255,204,102,1)';
-        case 63:
-            return 'rgba(136,204,204,1)';
-        default:
-            return 'rgba(0,0,255,1)';
-    }
-}
 
 
 function starta(): void {
@@ -145,8 +68,9 @@ function starta(): void {
     let fluffTecken = 0;
 
 
-    let generationLevel = new Array();
-    let generationLevelSymInfo = new Array();
+    let generationLevel = [];
+    let generationLevelSymInfo = [];
+    let generationLevelPolyomino = [];
     //att j�mf�ra med alla p� den f�reg�ende niv�n
     for (let v: number = 0; v < variantGeneration[variantGeneration.length - 1].length; v++) {//inre loop
         let paddedMatrix = createPaddedMatrix(variantGeneration[variantGeneration.length - 1][v], 0);
@@ -163,9 +87,10 @@ function starta(): void {
                         //om det inte finns N�T sparat f�r den leveln s� �r det bara att spara
                         if (generationLevel.length == 0) {
                             let matrixToStore = removePaddingFromMatrix(paddedMatrix, 0);
+
                             generationLevel.push(matrixToStore);
                             generationLevelSymInfo.push(calculateSymmetry(matrixToStore));
-                            //console.log("l�gger till ny 1");
+
                         }
                         else {
                             //j�mf�ra med alla p� befintlig generation level
@@ -177,14 +102,18 @@ function starta(): void {
                             }
                             if (!hittarMatchning) {//sparar om det inte finns n�n matchning
                                 let theSymmetryNumber = calculateSymmetry(matrixToCompare);
-                                generationLevelSymInfo.push(theSymmetryNumber);
+
                                 // om den �r enkelt symetrisk kring x-axeln eller kring 135 
                                 // grader roteras den 90 grader innan den sparas.
                                 if (theSymmetryNumber == 2 || theSymmetryNumber == 4) {
                                     //generationLevel.push(roteraMatrisMoturs90(matrixToCompare));
                                     generationLevel.push(rotate90CounterClockwise(matrixToCompare));
+                                    generationLevelSymInfo.push(theSymmetryNumber);
                                 }
-                                else { generationLevel.push(matrixToCompare); }
+                                else {
+                                    generationLevel.push(matrixToCompare);
+                                    generationLevelSymInfo.push(theSymmetryNumber);
+                                }
                             }
 
                         }
@@ -213,10 +142,12 @@ function starta(): void {
 
     //update info and repaint
     listChange();
+    drawShapes();
 
 
 
 }
+
 
 function calculateSymmetry(matris): number {
     let symmetryNumber: number = 0;
@@ -234,26 +165,13 @@ function calculateSymmetry(matris): number {
 }
 
 
-
-
-
-
-
-
-
-
-
-function minusKvadratBredd(): void {
-    if (kvadratBredd > 1) {
-        kvadratBredd--;
-    }
+function decreaseSize(): void {
+    view.decreaseCellSize();
     drawShapes();
 }
 
-function plusKvadratBredd(): void {
-    if (kvadratBredd < 40) {
-        kvadratBredd++;
-    }
+function increaseSize(): void {
+    view.increaseCellSize();
     drawShapes();
 }
 
@@ -276,7 +194,7 @@ function tock(): string {
 }
 
 function listChange(): void {
-    drawShapes();
+
     //antal delar
     document.getElementById('delar').innerHTML = getListValue() + '';
     //uppdaterar med antal
